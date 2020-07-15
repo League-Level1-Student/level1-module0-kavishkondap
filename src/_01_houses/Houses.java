d438d24b8e5117e3f53e6e0cd522d2c2e6587505
kavish09 <54075256+kavish09@users.noreply.github.com>
2020-07-15 13:23:11 -0700

    Level 1 Module 0 Houses Incomplete

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,30 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	static Robot bob = new Robot();
 	public void run() {
-		
+
+	bob.setX(10);
+	bob.setY(400);
+	for (int i = 1; i < 11; i++) {
+		drawHouse(15*i);
+	}
+	}
+	public static void drawHouse(int height) {
+		bob.setPenColor(0,0,0);
+		bob.penDown();
+		bob.setSpeed(75);
+		bob.setPenWidth(7);
+		bob.move(height);
+		bob.turn(90);
+		bob.move(50);
+		bob.turn(90);
+		bob.move(height);
+		bob.setPenColor(9, 79, 23);
+		bob.turn(-90);
+		bob.move(30);
+		bob.turn(-90);
 	}
 }
