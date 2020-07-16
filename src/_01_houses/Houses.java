0e24c1f002b44323fabe0f184489287310e787de
kavish09 <54075256+kavish09@users.noreply.github.com>
2020-07-16 13:21:44 -0700

    Level 1 Module 0 Drum Code

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -4,27 +4,77 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	static Robot bob = new Robot();
-	public void run() {
 
-	bob.setX(10);
-	bob.setY(400);
-	for (int i = 1; i < 11; i++) {
-		drawHouse(15*i);
+	public void run() {
+		bob.setWindowColor(0, 0, 0);
+		bob.setX(10);
+		bob.setY(400);
+		drawHouse("medium", "red");
+		drawHouse("large", "blue");
+		drawHouse("small", "green");
+		drawHouse("large", "random");
+		drawHouse("small", "random");
+		drawHouse("medium", "random");
+		drawHouse("large", "random");
+		drawHouse("small", "random");
+		
 	}
-	}
-	public static void drawHouse(int height) {
-		bob.setPenColor(0,0,0);
+
+	public static void drawHouse(String height, String color) {
+		if (color.equals("red")) {
+			bob.setPenColor(255, 0, 0);
+		} else if (color.equals("green")) {
+			bob.setPenColor(0, 255, 0);
+		} else if (color.equals("blue")) {
+			bob.setPenColor(0, 0, 255);
+		} else {
+			bob.setRandomPenColor();
+		}
+		int size;
+		if (height.equals("small")) {
+			size = 60;
+		} else if (height.equals("medium")) {
+			size = 120;
+		} else {
+			size = 250;
+		}
 		bob.penDown();
 		bob.setSpeed(75);
 		bob.setPenWidth(7);
-		bob.move(height);
-		bob.turn(90);
-		bob.move(50);
-		bob.turn(90);
-		bob.move(height);
+		bob.move(size);
+		if (size == 250) {
+			drawFlatRoof();
+		} else {
+			drawPointyRoof();
+		}
+		bob.move(size);
 		bob.setPenColor(9, 79, 23);
 		bob.turn(-90);
 		bob.move(30);
 		bob.turn(-90);
+		if (color.equals("red")) {
+			bob.setPenColor(255, 0, 0);
+		} else if (color.equals("green")) {
+			bob.setPenColor(0, 255, 0);
+		} else if (color.equals("blue")) {
+			bob.setPenColor(0, 0, 255);
+		} else {
+			bob.setRandomPenColor();
+		}
 	}
+
+	public static void drawPointyRoof() {
+		bob.turn(45);
+		bob.move(25);
+		bob.turn(90);
+		bob.move(25);
+		bob.turn(45);
+	}
+
+	public static void drawFlatRoof() {
+		bob.turn(90);
+		bob.move(50);
+		bob.turn(90);
+	}
+
 }
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -25,36 +25,47 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
-		// “Copy Image Address”)
-
+		
+String starbucks = "https://www.logogenie.net/images/articles/starbucks-logo1.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component c;
 		// 3. use the "createImage()" method below to initialize your Component
-
+c = createImage(starbucks);
 		// 4. add the image to the quiz window
-
+quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String answer1 = JOptionPane.showInputDialog(null,"What logo is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(answer1.equalsIgnoreCase("starbucks")){
+	JOptionPane.showMessageDialog(null, "CORRECT");
+}else {
+	JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+     quizWindow.remove(c);
 		// 10. find another image and create it (might take more than one line
 		// of code)
 
+String microsoft = "https://lh3.googleusercontent.com/proxy/0goCqylxX2lEXaOzYtc1aR49FbsVEoWrYnhgWO3U7nbBN5eXBQu3Ii_zZeunzOfDhIp0R5G4nvKF82PcTw8DicxwyMP2hViRGAIb9BbP9UMGJh0HmB2jNIaNVZpbyPuFsNVkJw";
+     Component d;
+d=createImage(microsoft);
 		// 11. add the second image to the quiz window
-
+quizWindow.add(d);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String answer2 = JOptionPane.showInputDialog(null, "Which logo is this?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if(answer2.equals("microsoft")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+	}else {
+		JOptionPane.showMessageDialog(null, "INCORRECT");
+	}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
